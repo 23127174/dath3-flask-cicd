73eb5ec8f64a806cd622ee6f5ae520e0e62664ef
23127174 <hmdung23@clc.fitus.edu.vn>
2026-01-08 23:47:17 +0700

    test-update

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "DATH#3 - CI/CD with Jenkins + Docker!"
+    return "DATH#3 - CI/CD with Jenkins + Docker! 23127174"
 
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
